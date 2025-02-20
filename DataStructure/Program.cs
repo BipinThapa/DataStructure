758a661db0013469834e08b39542e5493102ac34
bipin thapa <bipzzzlogin@gmail.com>
2025-02-20 15:16:19 +0545

    Add project files.

---
+++ DataStructure/Program.cs
@@ -0,0 +1,182 @@
+﻿namespace LearnDSAlgorithm
+{
+    //Lab Creating and Displaying Linked List
+    public class Node
+    {
+        public int element;
+        public Node next;
+        public Node(int e,Node n)
+        {
+            element = e;
+            next = n;
+        }
+    }
+    public class LinkedList
+    {
+        private Node head;
+        private Node tail;
+        private int size;
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+        public int length()
+        {
+            return size;
+        }
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+        #region Remove Element at first
+        public int removeFirstNode()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("List is Empty");
+                return - 1;
+            }
+            else
+            {
+                int e = head.element;
+                head = head.next;
+                size = size - 1;
+                if (isEmpty())
+                {
+                    tail = null;
+                   
+                }
+                return e;
+            }
+        }
+        #endregion
+
+        public int removeLastNode()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("List is Empty");
+                return -1;
+            }
+            Node p = head;
+            int i = 1;
+            while (i<length()-1)
+            {
+                p = p.next;
+                i = i + 1;
+            }
+            tail = p;
+            p = p.next;
+            int e = p.element;
+            tail.next = null;
+            size = size - 1;
+            return e;
+        }
+
+        #region Insert Element at any position
+        public void AddAny(int e, int position)
+        {
+            if (position <= 0 || position >=size)
+            {
+                Console.WriteLine("Invalid Position");
+                return;
+            }
+            Node newest = new Node(e, null);
+            Node p = head;
+            int i = 1;
+            while (i<position -1)
+            {
+                p = p.next;
+                i = i + 1;
+            }
+            newest.next = p.next;
+            p.next = newest;
+            size = size + 1;
+        }
+        #endregion
+
+        #region Insert Element at last of linked list
+        public void addLast(int e)
+        {
+            Node newest = new Node(e, null);
+            if (isEmpty())
+            {
+                head = newest;
+            }
+            else
+            {
+                tail.next = newest;
+            }
+            tail = newest;
+            size = size + 1;
+        }
+        #endregion
+
+        #region Insert Element at Beginning of linked list
+        public void addFirst(int e)
+        {
+            Node newest = new Node(e, null);
+            if (isEmpty())
+            {
+                head = newest;
+                tail = newest;
+            }
+            else
+            {
+                newest.next = head;
+                head = newest;
+            }
+            size = size + 1;
+        }
+        #endregion 
+        public void display()
+        {
+            Node p = head;
+            while (p!=null)
+            {
+                Console.Write(p.element + "--> ");
+                p = p.next;
+            }
+            Console.WriteLine();
+        }
+        public static void Main()
+        {
+            LinkedList l = new LinkedList();
+            l.addLast(7);
+            l.addLast(4);
+            l.addLast(12);
+            l.display();
+            Console.WriteLine("Size:"+l.length());
+            l.addLast(8);
+            l.addLast(3);
+            l.display();
+            Console.WriteLine("Size:" + l.length());
+            l.addFirst(15);
+            l.display();
+            Console.WriteLine("Size:" + l.length());
+            l.addFirst(25);
+            l.display();
+            Console.WriteLine("Size:" + l.length());
+            l.AddAny(20, 3);
+            l.display();
+            Console.WriteLine("Size:" + l.length());
+            l.AddAny(40,5);
+            l.display();
+            Console.WriteLine("Size:" + l.length());
+            //remove at first element
+            int element = l.removeFirstNode();
+            Console.WriteLine("Removed Element:" + element);
+            l.display();
+            Console.WriteLine("Size:" + l.length());
+            //remove at last element
+            int element1 = l.removeLastNode();
+            Console.WriteLine("Removed Element:" + element1);
+            l.display();
+            Console.WriteLine("Size:" + l.length());
+            Console.ReadKey();
+        }
+    }
+}
