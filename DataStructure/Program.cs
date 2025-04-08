a6c7e29daf768619368d74cab3a769c3821d3f31
bipin thapa <bipzzzlogin@gmail.com>
2025-04-09 00:24:47 +0545

    Head Recursion

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -2,38 +2,34 @@
 
 namespace DSAAlgorithm
 {
-    //Lab Iteration and Recursion
+    //Lab Head Recursion
     public class Recursion
     {
-        public void CalculateIterativeValue(int n)
-        {
-            while (n>0)
-            {
-                int k = n * n;
-                Console.WriteLine(k);
-                n = n - 1;
-            }
-        }
-
         public void CalculateRecursiveValue(int n)
         {
             if (n>0)
             {
+                CalculateRecursiveValue(n - 1);
                 int k = n * n;
                 Console.WriteLine(k);
-                CalculateRecursiveValue(n - 1);
             }
         }
         public static void Main()
         {
             Recursion r = new Recursion();
 
-            Console.WriteLine("Demo of Iterative function");
-            r.CalculateIterativeValue(4);
-
-            Console.WriteLine("Demo of Recursive function");
+            Console.WriteLine("Demo of Head Recursive function");
             r.CalculateRecursiveValue(4);
             Console.ReadKey();
         }
     }
 }
+
+/*
+   Output:
+   Demo of Head Recursive function
+   1
+   4
+   9
+   16
+*/
