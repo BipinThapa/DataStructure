d2f34c92b21f20c00b107e4f6ea188c696724304
bipin thapa <bipzzzlogin@gmail.com>
2025-04-22 12:17:02 +0545

    shellsort

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -7,18 +7,20 @@ namespace DSAlgorithms
 {
     class Sort
     {
-        public void bubblesort(int[] A, int n)
+        public void shellsort(int[] A, int n)
         {
-            for (int pass = n - 1; pass >= 0; pass--)
+            for (int gap = n/2; gap > 0; gap=gap/2)
             {
-                for (int i = 0; i < pass; i++)
+                for (int i = gap; i < n; i++)
                 {
-                    if (A[i] > A[i + 1])
+                    int gvalue = A[i];
+                    int j = i - gap;
+                    while (j >= 0 && A[j] > gvalue)
                     {
-                        int temp = A[i];
-                        A[i] = A[i + 1];
-                        A[i + 1] = temp;
+                        A[j + gap] = A[j];
+                        j = j - gap;
                     }
+                    A[j + gap] = gvalue;
                 }
             }
         }
@@ -34,12 +36,34 @@ namespace DSAlgorithms
         {
             Sort s = new Sort();
             int[] A = { 3, 5, 8, 9, 6, 2 };
-            Console.WriteLine("Original Array:");
+            Console.WriteLine("Original Array: ");
             s.display(A, 6);
-            s.bubblesort(A, 6);
-            Console.WriteLine("Sorted Array: ");
+            s.shellsort(A, 6);
+            Console.WriteLine("Sorted Array:");
             s.display(A, 6);
             Console.ReadKey();
         }
     }
 }
+
+
+/*
+  int gap = n / 2;
+            while (gap > 0)
+            {
+                int i = gap;
+                while (i < n)
+                {
+                    int temp = A[i];
+                    int j = i - gap;
+                    while (j >= 0 && A[j] > temp)
+                    {
+                        A[j + gap] = A[j];
+                        j = j - gap;
+                    }
+                    A[j + gap] = temp;
+                    i = i + 1;
+                }
+                gap = gap / 2;
+            }
+ */
